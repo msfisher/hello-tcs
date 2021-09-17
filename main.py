e0da5fd27b097beeb9863bd693c118c775cbdcde
BuildTools <unconfigured@null.spigotmc.org>
2021-09-17 15:23:36 -0400

    added feature to get user's name

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # simple python program to print hello
 # this is used for Git / GitHub demonstration
+import random
 
-name = input("Please enter your name: ")
+sayings = ["hola", "Bonjour", "Hello"]
 
-print("Hello " + name + " @ TCS")  # print to the screen
+def sayHello(name):
+    index = random.randint(0,3)
+    print(sayings[index] + " " + name)
+
+firstName = input("Please enter your name: ")
+sayHello(firstName)
+print(" @ TCS")  # print to the screen
 
